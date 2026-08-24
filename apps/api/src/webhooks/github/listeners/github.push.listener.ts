caebebb106ac82633247df21f7e593b145b22faa
safeone1 <safeonekan@gmail.com>
2026-08-24 22:22:51 +0100

    test push 1

--- apps/api/src/webhooks/github/listeners/github.push.listener.ts
+++ apps/api/src/webhooks/github/listeners/github.push.listener.ts
@@ -5,11 +5,7 @@ import type { PushEvent } from '@octokit/webhooks-types';
 @Injectable()
 export class GithubPushListener {
   @OnEvent('github.push')
-  handlePushEvent(payload: PushEvent) {
-    console.log(
-      '[github.push] event received:',
-      payload.pusher.username,
-      payload.repository.full_name,
-    );
+  handlePushEvent(payload: any) {
+    console.log('[github.push] event received:', payload);
   }
 }
